12e65646dfac8cfb7621a6940de72cfb6ca26b10
Huisu Lim <4922865@gmail.com>
2021-07-13 00:13:50 +0900

    change library zip

--- Thrust.h
+++ Thrust.h
@@ -1,5 +1,48 @@
+#include "HX711.h"
 const int setSw=4;
+const int DOUT=3; //dataPin
+const int CLK=2;//clockPin 
 
+HX711 scale(DOUT, CLK);
+
+float calibration_factor = -7050;    //로드셀 종류나 상황에 따라 적당한 값으로 시작
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("HX711 calibration sketch");
+  Serial.println("Remove all weight from scale");
+  Serial.println("After readings begin, place known weight on scale");
+  Serial.println("Press + or a to increase calibration factor");
+  Serial.println("Press - or z to decrease calibration factor");
+
+  scale.set_scale();
+  scale.tare();	//Reset the scale to 0
+
+  long zero_factor = scale.read_average(); //Get a baseline reading
+  Serial.print("Zero factor: "); //This can be used to remove the need to tare the scale. Useful in permanent scale projects.
+  Serial.println(zero_factor);
+}
+
+void loop() {
+
+  scale.set_scale(calibration_factor); //Adjust to this calibration factor
+
+  Serial.print("Reading: ");
+  Serial.print(scale.get_units(), 1);
+  Serial.print(" kg"); //Change this to kg and re-adjust the calibration factor if you follow SI units like a sane person 우리 한국인은 모두 이성적이므로^^
+  Serial.print(" calibration_factor: ");
+  Serial.print(calibration_factor);
+  Serial.println();
+
+  if(Serial.available())
+  {
+    char temp = Serial.read();
+    if(temp == '+' || temp == 'a')
+      calibration_factor += 10;
+    else if(temp == '-' || temp == 'z')
+      calibration_factor -= 10;
+  }
+}
 
 void setInit0(){
   bool sw=true;
